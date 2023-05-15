f5a6eb62e6739e963705791bebf4a63f012f9330
HaoZhang <zhanghao997@qq.com>
2023-05-16 04:14:16 +0800

    use multiple clients

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,12 +3,20 @@ package org.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
+import software.amazon.awssdk.core.client.config.SdkAdvancedAsyncClientOption;
+import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
+import software.amazon.awssdk.http.nio.netty.SdkEventLoopGroup;
+import software.amazon.awssdk.regions.Region;
+
 import java.util.Vector;
 
 public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
     private static final int run_seconds = 30;
     private static final int report_interval = 5;
+    private static final int num_clients = 2;
+    private static final int pool_size = 2;
 
     public static void main(String... args) {
         String cmd = args[0];
@@ -22,8 +30,18 @@ public class App {
             }
         } else if (cmd.equals("async")) {
             int req_per_thd = Integer.parseInt(args[2]);
+            AsyncWorker.clients = new DynamoDbAsyncClient[num_clients];
+            for (int i = 0; i < num_clients; ++i) {
+                AsyncWorker.clients[i] = DynamoDbAsyncClient.builder()
+                        .region(Region.AP_NORTHEAST_1).endpointOverride(Worker.endpoint).asyncConfiguration(
+                                b -> b.advancedOption(SdkAdvancedAsyncClientOption.FUTURE_COMPLETION_EXECUTOR,
+                                        Runnable::run))
+                        .httpClientBuilder(NettyNioAsyncHttpClient.builder()
+                                .eventLoopGroupBuilder(SdkEventLoopGroup.builder().numberOfThreads(pool_size)))
+                        .build();
+            }
             for (int i = 0; i < num_thds; ++i) {
-                AsyncWorker w = new AsyncWorker(req_per_thd);
+                AsyncWorker w = new AsyncWorker(i, req_per_thd);
                 w.start();
                 workers.add(w);
             }
--- src/main/java/org/example/AsyncWorker.java
+++ src/main/java/org/example/AsyncWorker.java
@@ -4,29 +4,21 @@ import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
 import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.GetItemResponse;
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
-import software.amazon.awssdk.core.client.config.SdkAdvancedAsyncClientOption;
-import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
-import software.amazon.awssdk.http.nio.netty.SdkEventLoopGroup;
-import software.amazon.awssdk.regions.Region;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.Condition;
 
 public class AsyncWorker extends Worker {
-    private final static DynamoDbAsyncClient client = DynamoDbAsyncClient.builder()
-            .region(Region.AP_NORTHEAST_1).endpointOverride(endpoint).asyncConfiguration(
-                    b -> b.advancedOption(SdkAdvancedAsyncClientOption.FUTURE_COMPLETION_EXECUTOR,
-                            Runnable::run))
-            .httpClientBuilder(NettyNioAsyncHttpClient.builder()
-                    .eventLoopGroupBuilder(SdkEventLoopGroup.builder().numberOfThreads(8)))
-            .build();
+    public static DynamoDbAsyncClient clients[];
+    private DynamoDbAsyncClient client;
     private ReentrantLock mu = new ReentrantLock();
     private Condition cv = mu.newCondition();
     private int inflight = 0;
     private final int maxInflight;
 
-    AsyncWorker(int maxf) {
+    AsyncWorker(int id, int maxf) {
+        client = clients[id % clients.length];
         maxInflight = maxf;
     }
 
